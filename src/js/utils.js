e6cebc08fbf6c60b7457d77adefd436b4d5c60dc
Thiago Cardoso <intelxd@gmail.com>
2018-10-03 09:35:54 -0300

    Change job row structure

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,18 +5,18 @@ let alerttmpl = '<div class="alert alert-{{classtype}} alert-dismissible fade sh
 let badgetmpl = '<span class="badge btn-block badge-{{classtype}}" style="margin:0px;">{{message}}</span>'
 let rowtmpl = '<tr class="row">'
             + '  <th class="col-2" scope="row">{{createdin}}</th>'
-            + '  <td class="col-2">{{jobtype}}</td>'
-            + '  <td class="col-1">{{jobid}}</td>'
+            + '  <td class="col-2 jobtype">{{jobtype}}</td>'
+            + '  <td class="col-1 jobid">{{jobid}}</td>'
             + '  <td class="col-2">{{owner}}</td>'
             + '  <td class="col-1" style="display:flex;align-items:center">{{status}}</td>'
             + '  <td class="col-2" style="display:flex;align-items:center">{{progress}}</td>'
             + '  <td class="col-2">{{btns}}</td>'
             + '</tr>'
-let btnactionstmpl = '<button type="button" class="btn btn-outline-dark btn-sm" data-toggle="tooltip" data-placement="bottom" title="Fetch status"><i class="fas fa-sync-alt"></i></button>\n'
-                   + '<button type="button" class="btn btn-outline-dark btn-sm" data-toggle="tooltip" data-placement="bottom" title="Open job"><i class="fas fa-external-link-alt"></i></button>\n'
-                   + '<button type="button" class="btn btn-outline-dark btn-sm js-copy-btn" copy-link="test1" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Copy Tapas link"><i class="far fa-copy"></i></button>\n'
-                   + '<button type="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Unfollow this job"><i class="far fa-trash-alt"></i></button>\n'
-                   + '<button type="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="bottom" title="Show detailed info"><i class="fas fa-info"></i></button>\n'
+let btnactionstmpl = '<!--<button type="button" class="btn btn-outline-dark btn-sm" data-toggle="tooltip" data-placement="bottom" title="Fetch status"><i class="fas fa-sync-alt fa-fw"></i></button>\n-->'
+                   + '<button type="button" class="btn btn-outline-dark btn-sm" data-toggle="tooltip" data-placement="bottom" title="Open job"><i class="fas fa-external-link-alt fa-fw"></i></button>\n'
+                   + '<button type="button" class="btn btn-outline-dark btn-sm js-copy-btn" copy-link="test1" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Copy Tapas link"><i class="far fa-copy fa-fw"></i></button>\n'
+                   + '<button type="button" class="btn btn-outline-danger btn-sm delete-job-btn" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Unfollow this job"><i class="far fa-trash-alt fa-fw"></i></button>\n'
+                   + '<button type="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="bottom" title="Show detailed info"><i class="fas fa-info fa-fw"></i></button>\n'
 // let progbartmpl = '<div class="progress" style="height:100%">'
 let progbartmpl = '<div class="progress" style="margin:0px;width: 100%">'
                + '  <div class="progress-bar {{status}}" role="progressbar" style="font-size:.9em;width:{{width}}%" aria-valuenow="{{value}}" aria-valuemin="0" aria-valuemax="100">{{value}}%</div>'
@@ -46,7 +46,7 @@ let addJobRow = (jobs) => {
     let progbar = createProgbar(Math.round(job.progress * 100))
     let btns = btnactionstmpl
     let jobrow = rowtmpl.replace('{{createdin}}', job.createdIn)
-        .replace('{{jobtype}}', job.jobtype)
+        .replace('{{jobtype}}', job.jobname)
         .replace('{{jobid}}', job.jobid)
         .replace('{{owner}}', job.owner)
         .replace('{{status}}', status)
